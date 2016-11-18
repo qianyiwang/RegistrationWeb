5ec8fcde7ee33970ef4a611ccff17b80576d45cf
wang qianyi <wangqianyi@wangs-MacBook-Air.local>
2016-11-17 23:40:58 -0500

    delet

--- registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-$umid = $_GET["umid"];
-$fname = $_GET["fname"];
-$lname = $_GET["lname"];
-$projtitle = $_GET["projtitle"];
-$email = $_GET["email"];
-$phone = $_GET["phone"];
-echo "Thank you " . $name . ". Your project title is: " . $projtitle;
-?>
