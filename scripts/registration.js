30e3dc9be12fbf40aa0be7e27da2e573b74bfe81
wang qianyi <wangqianyi@wangs-MacBook-Air.local>
2016-11-26 21:40:33 -0500

    complete front information error check

--- scripts/registration.js
+++ scripts/registration.js
@@ -5,6 +5,14 @@ var $ = function(id) {
 
 function initial() {
     repeatFlag = false;
+
+    $('umid').oninput = function() {reset('umid')};
+    $('fname').oninput = function() {reset('fname')};
+    $('lname').oninput = function() {reset('lname')};
+    $('pname').oninput = function() {reset('pname')};
+    $('email').oninput = function() {reset('email')};
+    $('phone').oninput = function() {reset('phone')};
+
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
@@ -12,7 +20,7 @@ function initial() {
             var obj = JSON.parse(json);
             var studentNumArr = obj.studentnum;
             umidArr = obj.umid;
-            $("result").innerHTML = studentNumArr + umidArr;
+            //$("result").innerHTML = studentNumArr + umidArr;
 
             // initial select menu
             var select = $("slotSelectMenu");
@@ -23,42 +31,42 @@ function initial() {
                         output += "<option ";
                         output += "value="+i+">";
                         output += "12/9/15, 6:00 PM – 7:00 PM, ";
-                        output += 6-studentNumArr[i];
+                        output += 6-studentNumArr[i]+" seats remaining";
                         output += "</option>";
                         break;
                     case 1:
                         output += "<option ";
                         output += "value="+i+">";
                         output += "12/9/15, 7:00 PM – 8:00 PM, ";
-                        output += 6-studentNumArr[i];
+                        output += 6-studentNumArr[i]+" seats remaining";
                         output += "</option>";
                         break;
                     case 2:
                         output += "<option ";
                         output += "value="+i+">";
                         output += "12/9/15, 8:00 PM – 9:00 PM, ";
-                        output += 6-studentNumArr[i];
+                        output += 6-studentNumArr[i]+" seats remaining";
                         output += "</option>";
                         break;
                     case 3:
                         output += "<option ";
                         output += "value="+i+">";
                         output += "12/10/15, 6:00 PM – 7:00 PM, ";
-                        output += 6-studentNumArr[i];
+                        output += 6-studentNumArr[i]+" seats remaining";
                         output += "</option>";
                         break;
                     case 4:
                         output += "<option ";
                         output += "value="+i+">";
                         output += "12/10/15, 7:00 PM – 8:00 PM, ";
-                        output += 6-studentNumArr[i];
+                        output += 6-studentNumArr[i]+" seats remaining";
                         output += "</option>";
                         break;
                     case 5:
                         output += "<option ";
                         output += "value="+i+">";
                         output += "12/10/15, 8:00 PM – 9:00 PM, ";
-                        output += 6-studentNumArr[i];
+                        output += 6-studentNumArr[i]+" seats remaining";
                         output += "</option>";
                         break;
 
@@ -71,24 +79,76 @@ function initial() {
     xmlhttp.send();
 }
 
+function checkFormat(){
+  var flag = true;
+  var umid = $('umid').value;
+  var email = $('email').value;
+  var phone = $('phone').value;
+  var fname = $('fname').value;
+  var lname = $('lname').value;
+  var pname = $('pname').value;
+  var umidRegex = /^\d{8}$/;
+  var emailRegex = /^\S{1,55}[@]\S{1,20}[\.]\S{1,3}$/;
+  var phoneRegex = /^\d{3}[\-]\d{3}[\-]\d{4}$/;
+  if(!umidRegex.test(umid)){
+    $('umid').setAttribute("class", "error_highlight");
+    $('umid_error_message').innerHTML = "UMID needs to be 8 digits long";
+    flag = false;
+  }
+  if(fname==""){
+    $('fname').setAttribute("class", "error_highlight");
+    $('fname_error_message').innerHTML = "First name cannot be empty";
+    flag = false;
+  }
+  if(lname==""){
+    $('lname').setAttribute("class", "error_highlight");
+    $('lname_error_message').innerHTML = "Last name cannot be empty";
+    flag = false;
+  }
+  if(pname==""){
+    $('pname').setAttribute("class", "error_highlight");
+    $('pname_error_message').innerHTML = "Project name cannot be empty";
+    flag = false;
+  }
+  if(!emailRegex.test(email)){
+    $('email').setAttribute("class", "error_highlight");
+    $('email_error_message').innerHTML = "Email format: example@domain.com";
+    flag = false;
+  }
+  if(!phoneRegex.test(phone)){
+    $('phone').setAttribute("class", "error_highlight");
+    $('phone_error_message').innerHTML = "Phone format: 999-999-9999";
+    flag = false;
+  }
+
+  return flag;
+}
+
+function reset(name){
+  $(name).setAttribute("class", "normal");
+  $(name+'_error_message').innerHTML = "";
+}
+
 function checkRepeat(){
     for(var i=0; i<umidArr.length; i++){
         // console.log($("umid").value);
         // console.log(umidArr[i]);
         if(umidArr[i].includes($("umid").value)){
-            return true;
+            return false;
         }
     }
-    return false;
+    return true;
 }
 
 function checkform(){
-    console.log(checkRepeat());
-    if(checkRepeat()){ 
-        condition = confirm('You have registered. Do you want to update?'); 
-        return condition;
-    }
-    return true;
+  if(!checkFormat()){
+    return false;
+  }
+  if(checkRepeat()){
+      condition = confirm('You have registered. Do you want to update?');
+      return condition;
+  }
+  return true;
 }
 
 window.addEventListener("load", initial);
--- scripts/registration.php
+++ scripts/registration.php
@@ -8,28 +8,63 @@ $phone = $_GET["phone"];
 $select = $_GET["select"]+1;
 
 // initial db, select table
-$mysql_server_name="198.71.225.61"; 
-$mysql_username="qianyiw"; 
-$mysql_password="123456789"; 
-$mysql_database="RegisterDB"; 
+$mysql_server_name="198.71.225.61";
+$mysql_username="qianyiw";
+$mysql_password="123456789";
+$mysql_database="RegisterDB";
 $con=mysql_connect($mysql_server_name, $mysql_username,
                                 $mysql_password);
 
 mysql_select_db("RegisterDB",$con);
 
+// check and remove repeat information
+removePrev();
 // insert into row
 fetchRowAndUpdate($select);
-// else{
-// 	echo "<script type='text/javascript'> 
-// 	answer = confirm('You have already registered, do you want to update?'); 
-// 	</script>";
-// 	// fetchRowAndUpdate($select);
-// }
 
 mysql_close($con);
 
 function removePrev(){
+  $result = mysql_query("SELECT * FROM TimeSlot");
+  while ($row = mysql_fetch_array($result))
+  {
+      if(strpos($row["umid"],$GLOBALS['umid'])!== false){
+        // convert information string to array
+        $idx = $row["id"];
+        $n = $row["studentnum"];
+        $umidArray = explode(',', $row["umid"]);
+        $fnameArray = explode(',', $row["fname"]);
+        $lnameArray = explode(',', $row["lname"]);
+        $pnameArray = explode(',', $row["project name"]);
+        $emailArray = explode(',', $row["email"]);
+        $phoneArray = explode(',', $row["phone"]);
+        // // remove array element
+        $n = $n-1;
+        for($i = 0; $i < sizeof($umidArray);$i++){
+          if($GLOBALS['umid']==$umidArray[$i]){
+            array_splice($umidArray, $i, 1);
+            array_splice($fnameArray, $i, 1);
+            array_splice($lnameArray, $i, 1);
+            array_splice($pnameArray, $i, 1);
+            array_splice($emailArray, $i, 1);
+            array_splice($phoneArray, $i, 1);
+          }
+        }
+        // convert array to string
+        $temp_umid = implode(',', $umidArray);
+        $temp_fname = implode(',', $fnameArray);
+        $temp_lname = implode(',', $lnameArray);
+        $temp_pname = implode(',', $pnameArray);
+        $temp_email = implode(',', $emailArray);
+        $temp_phone = implode(',', $phoneArray);
+        // update DB
+        mysql_query("UPDATE `TimeSlot` SET `studentnum` = '$n', `umid` = '$temp_umid', `fname` = '$temp_fname',
+      	`lname` = '$temp_lname', `project name` = '$temp_pname', `email` = '$temp_email', `phone` = '$temp_phone'
+      	WHERE id = '$idx' ");
 
+        break;
+      }
+  }
 }
 
 function fetchRowAndUpdate($select){
@@ -64,8 +99,8 @@ function fetchRowAndUpdate($select){
 		}
 	}
 
-	mysql_query("UPDATE `TimeSlot` SET `studentnum` = '$studentnum', `umid` = '$totalUmid', `fname` = '$totalFirstName', 
-	`lname` = '$totalLastName', `project name` = '$totalProj', `email` = '$totalEmail', `phone` = '$totalPhone' 
+	mysql_query("UPDATE `TimeSlot` SET `studentnum` = '$studentnum', `umid` = '$totalUmid', `fname` = '$totalFirstName',
+	`lname` = '$totalLastName', `project name` = '$totalProj', `email` = '$totalEmail', `phone` = '$totalPhone'
 	WHERE id = '$select' ");
 }
 
@@ -81,20 +116,16 @@ function fetchRowAndUpdate($select){
 	<header>
 		<h1><?php echo $fname ?>, please view your registration information.</h1>
 	</header>
-		
+
 	<main>
 		<h2><?php echo $fname ?>'s information: </h2>
-		
+
 
 		<h2>These students are also in your time slot: </h2>
-		
+
 	</main>
 	<footer>
     	<p>&#169 2016 Qianyi Wang</p>
     </footer>
 </body>
 </html>
-
-
-
-
